a159183b3d9447d973fa7aa0f2898c2dd14ae2ad
Marco Hladik <marco@icculus.org>
2020-04-03 15:29:24 +0200

    Footsteps: Ladder sound fix

--- src/server/footsteps.c
+++ src/server/footsteps.c
@@ -147,7 +147,7 @@ Footsteps_Update(void)
 		}
 
 		/* make it so we step once we land */
-		if (!(self.flags & FL_ONGROUND)) {
+		if (!(self.flags & FL_ONGROUND) && !(self.flags & FL_ONLADDER)) {
 			self.fStepTime = 0.0f;
 			return;
 		}
