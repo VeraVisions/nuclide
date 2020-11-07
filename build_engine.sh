c737cebba71e47a5e4ee74abfb9d03259258d7bb
Marco Hladik <marco@icculus.org>
2020-11-07 10:40:36 +0100

    build_engine.sh: build iqm and imgtool

--- build_engine.sh
+++ build_engine.sh
@@ -6,13 +6,13 @@ FTE_MAKEFILE=./src/engine/engine/Makefile
 mkdir -p ./bin
 
 if [ -f "$FTE_MAKEFILE" ]; then
-    printf "Engine is present, updating...\n"
-    cd ./src/engine/
+	printf "Engine is present, updating...\n"
+	cd ./src/engine/
 	svn up
 	cd ./engine
 else
-    printf "Engine is NOT present, cloning...\n"
-    cd ./src/
+	printf "Engine is NOT present, cloning...\n"
+	cd ./src/
 	svn checkout https://svn.code.sf.net/p/fteqw/code/trunk engine
 	cd ./engine/engine
 fi
@@ -24,6 +24,10 @@ make -j $(nproc) sv-rel
 cp -v ./release/fteqw-sv ../../../bin/fteqw-sv
 make -j $(nproc) qcc-rel
 cp -v ./release/fteqcc ../../../bin/fteqcc
+make -j $(nproc) iqm-rel
+cp -v ./release/iqm ../../../bin/iqm
+make -j $(nproc) imgtool-rel
+cp -v ./release/imgtool ../../../bin/imgtool
 make -j $(nproc) plugins-rel NATIVE_PLUGINS="bullet"
 find ./release/ -name 'fteplug_bullet_*.so' -exec cp -prv '{}' '../../../bin/' ';'
 make -j $(nproc) plugins-rel NATIVE_PLUGINS="ffmpeg"
