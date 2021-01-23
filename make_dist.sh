d5ff5d870c7ed85bd41ccab30c80d519e50c605a
Marco Hladik <marco@icculus.org>
2021-01-23 22:51:25 +0100

    make_dist.sh: Use .zip for packaging, write new installed.lst instead of copying a working/modified one

--- make_dist.sh
+++ make_dist.sh
@@ -42,11 +42,15 @@ copy_file logos/fte.png
 touch ./_release/fteqw_goes_here
 mv ./_release/doc/release-readme ./_release/README
 cp ./valve.fmf ./_release/default.fmf
-cp ./installed.lst ./_release/installed.lst
+echo "version 2" > ./_release/installed.lst
+echo "set updatemode \"1\"" >> ./_release/installed.lst
+echo "set declined \"\"" >> ./_release/installed.lst
+echo "sublist \"http://www.frag-net.com/dl/valve_packages\" \"\" \"enabled\"" >> ./_release/installed.lst
+
 rm -rf ./_release/platform/test_maps.pk3dir
 
 # build the tarball
 export FILE_OUT=nuclide-$(date +%m-%d-%Y)
 mv ./_release "./$FILE_OUT"
-tar zcvf "$FILE_OUT".tar.gz "./$FILE_OUT"
-gpg --output "./$FILE_OUT.sig" --detach-sig "./$FILE_OUT.tar.gz"
+zip -9 -r "$FILE_OUT".zip "./$FILE_OUT"
+gpg --output "./$FILE_OUT.sig" --detach-sig "./$FILE_OUT.zip"
