3e13cb46ffc6ab1591ef70666c2b145a1acb788f
Marco Hladik <marco@icculus.org>
2021-02-20 12:01:27 +0100

    build_game.sh: script will now build QuakeC progs for any detected mod subdirectories

--- build_game.sh
+++ build_game.sh
@@ -4,8 +4,12 @@ SCRPATH="$( cd "$( dirname $(readlink -nf $0) )" && pwd )"
 PATH="$SCRPATH"/bin:"$PATH"
 
 if [ -f "$SCRPATH"/bin/fteqcc ]; then
-	cd ./src
-	make
+	export OLDDIR=$(pwd)
+	find -name Makefile | grep 'src\/Makefile' | grep -v engine | while read MFILE_N; do
+		cd $(dirname $MFILE_N)
+		make
+		cd $OLDDIR
+	done;
 else
 	printf "FTEQCC compiler is not present, please run build_engine.sh\n"
 fi
