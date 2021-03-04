568121d05fd478e89c40b7d754e0128d934d1ec0
Marco Hladik <marco@icculus.org>
2021-03-04 05:04:14 +0100

    Update build_editor.sh, build_game.sh and make_mapdef.sh to generate individual eclass .def files for Radiant

--- build_game.sh
+++ build_game.sh
@@ -8,7 +8,10 @@ if [ -f "$SCRPATH"/bin/fteqcc ]; then
 	find "$SCRPATH" -name Makefile | grep 'src\/Makefile' | grep -v engine | while read MFILE_N; do
 		cd $(dirname $MFILE_N)
 		make
+		cd ..
+		export GAMEDIR=$(basename $PWD)
 		cd $OLDDIR
+		./make_mapdef.sh $GAMEDIR
 	done;
 else
 	printf "FTEQCC compiler is not present, please run build_engine.sh\n"
--- make_mapdef.sh
+++ make_mapdef.sh
@@ -2,20 +2,22 @@
 
 ent_for_mod()
 {
-    mkdir -p ./bin/$1.game/$1/
-    ENT_OUTFILE="./bin/$1.game/$1/entities.def"
+    mkdir -p ./bin/platform.game/$1/
+    ENT_OUTFILE="./bin/platform.game/$1/entities.def"
     rm "$ENT_OUTFILE"
 
-    find ./$1/src/ -type f \( -iname \*.cpp -o -iname \*.c \) | while read EDEF_N; do
+    find ./$1/src/ -type f \( -iname \*.qc \) | while read EDEF_N; do
         echo "Scanning for definitions inside $EDEF_N"
         sed -n '/\/*QUAKED/,/*\//p' $EDEF_N >> "$ENT_OUTFILE"
     done;
+    
+    cat ./bin/platform.game/platform/entities.def >> $ENT_OUTFILE
 }
 
-BASE_ENT=./bin/platform.game/platform/entities_base.def
+BASE_ENT=./bin/platform.game/platform/entities.def
 rm "$BASE_ENT"
 
-find ./src/gs-entbase/ -type f \( -iname \*.cpp -o -iname \*.c \) | while read EDEF_N; do
+find ./src/gs-entbase/ -type f \( -iname \*.qc \) | while read EDEF_N; do
     echo "Scanning for definitions inside $EDEF_N"
     sed -n '/\/*QUAKED/,/*\//p' $EDEF_N >> "$BASE_ENT"
 done;
