8b4cd6b8c78eb178e5562f0c890252f64a02cdbe
Marco Hladik <marco@icculus.org>
2021-03-06 01:20:25 +0100

    build_game.sh: Run make for menu/plugins and check if the level-editor is present before calling make_mapdef.sh

--- build_game.sh
+++ build_game.sh
@@ -5,13 +5,19 @@ PATH="$SCRPATH"/bin:"$PATH"
 
 if [ -f "$SCRPATH"/bin/fteqcc ]; then
 	export OLDDIR=$(pwd)
+	cd ./src
+	make
+	cd "$OLDDIR"
+
 	find "$SCRPATH" -name Makefile | grep 'src\/Makefile' | grep -v engine | while read MFILE_N; do
 		cd $(dirname $MFILE_N)
 		make
 		cd ..
 		export GAMEDIR=$(basename $PWD)
 		cd $OLDDIR
+		if [ -f "$SCRPATH"/bin/worldspawn ]; then
 		./make_mapdef.sh $GAMEDIR
+		fi
 	done;
 else
 	printf "FTEQCC compiler is not present, please run build_engine.sh\n"
