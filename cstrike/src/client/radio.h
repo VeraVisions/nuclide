12286e5032d1b2266e725c6f56aa27067d5bcb28
Marco Hladik <marco@icculus.org>
2021-03-08 10:42:19 +0100

    Exclude mod-dirs (except for base) from Nuclide's source tree

--- cstrike/src/client/radio.h
+++
@@ -1,3 +0,0 @@
-void Radio_Init(void);
-void Radio_PlayMessage(float);
-void Radio_PlayPlayerMessage(float, float);
--- cstrike/src/server/radio.h
+++
@@ -1,4 +0,0 @@
-void Radio_BroadcastMessage(float fMessage)
-void Radio_TeamMessage(float fMessage, float fTeam)
-float Radio_DefaultStart(void)
-void Radio_StartMessage(void) 
