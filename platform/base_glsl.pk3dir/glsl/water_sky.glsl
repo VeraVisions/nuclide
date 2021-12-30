f0e1c3b9e1065c43c552da3776cd400f2c91e0b8
Marco Hladik <marco@icculus.org>
2021-12-30 15:36:26 -0800

    GLSL: Go over water_sky.glsl, make it work for TW's upcoming tw_rig.

--- platform/base_glsl.pk3dir/glsl/water_sky.glsl
+++ platform/base_glsl.pk3dir/glsl/water_sky.glsl
@@ -1,13 +1,14 @@
-//======= Copyright (c) 2015-2020 Vera Visions LLC. All rights reserved. =======
+//======= Copyright (c) 2015-2022 Vera Visions LLC. All rights reserved. =======
 //
 // Purpose: 
 //
-// Water shader (normalmap only) where there's only refraction.
+// Water shader that distorts the reflection based on a supplied normalmap
+// and blends it on top of a skybox (cube) image.
 //==============================================================================
 
 !!ver 110
 !!permu FOG
-!!samps 2 diffuse normalmap reflectcube
+!!samps reflect=0 norm=1 skycube:samplerCube=2
 
 #include "sys/defs.h"
 
@@ -28,7 +29,9 @@ varying vec2 shift2;
 		invsurface[1] = v_tvector;
 		invsurface[2] = v_normal;
 		eyeminusvertex = e_eyepos - v_position.xyz;
-
+		eyevector.x = dot(eyeminusvertex, v_svector.xyz);
+		eyevector.y = dot(eyeminusvertex, v_tvector.xyz);
+		eyevector.z = dot(eyeminusvertex, v_normal.xyz);
 
 		tf_c = ftetransform();
 		tf_c.z += 0.1;
@@ -48,24 +51,29 @@ varying vec2 shift2;
 		vec3 refl_f;
 		vec3 refr_f;
 		vec3 norm_f;
+		vec3 cube_c;
 		vec4 out_f = vec4( 1.0, 1.0, 1.0, 1.0 );
 
 		// Use the normalmap to shift the refraction
-		norm_f = ( texture2D( s_normalmap, shift1 ).xyz);
-		norm_f += ( texture2D( s_normalmap, shift2 ).xyz);
-		norm_f -= 1.0 - ( 4.0 / 1024.0 );
-		norm_f = normalize( norm_f ) * 0.1;
+		norm_f = ( texture2D( s_norm, shift1 ).xyz);
+		norm_f += ( texture2D( s_norm, shift2 ).xyz);
+		norm_f -= 1.0 - ( 16.0 / 1024.0 );
+		norm_f = normalize( norm_f );
 
 		// Reflection/View coordinates
 		refl_c = ( 1.0 + ( tf_c.xy / tf_c.w ) ) * 0.5;
 		refl_c.t -= 1.5 * invsurface[2].z / 1080.0;
 
-			// Load reflection and refraction based on our new coords
-		refl_f = texture2D( s_t0, refl_c).rgb;
-		refr_f = texture2D( s_t1, refl_c + ( norm_f.st * 0.1 ) ).rgb;
+		// Load reflection and refraction based on our new coords
+		refl_f = texture2D(s_reflect, refl_c + ( norm_f.st * 0.1 )).rgb;
 
-		fres = pow( 1.0 - abs( dot( norm_f, normalize( eyeminusvertex ) ) ), 5.0 );
-		out_f.rgb = refr_f;
+		cube_c = reflect(normalize(eyevector), norm_f.rgb);
+		cube_c = cube_c.x * invsurface[0] + cube_c.y * invsurface[1] + cube_c.z * invsurface[2];
+		cube_c = (m_model * vec4(cube_c.xyz, 0.0)).xyz;
+		refr_f = textureCube(s_skycube, cube_c).rgb;
+
+		fres = pow( 1.0 - abs( dot( norm_f, normalize( eyeminusvertex ) ) ), 1.0 );
+		out_f.rgb = mix( refr_f, refl_f, fres );
 
 		gl_FragColor = fog4(out_f);
 	}
