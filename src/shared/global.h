bda01e0f0f44a4a5b8d82544fbeffc69697cccce
Marco Cawthorne <marco@vera-visions.com>
2022-07-20 16:48:41 -0700

    Shared: NSLog should just take whatever N args

--- src/shared/global.h
+++ src/shared/global.h
@@ -20,7 +20,7 @@ _NSLog(string msg)
 	if (cvar("g_developer") == 1)
 		print(sprintf("%f %s\n", time, msg));
 }
-#define NSLog(x, ...) _NSLog(sprintf(x, __VA_ARGS__))
+#define NSLog(...) _NSLog(sprintf(__VA_ARGS__))
 
 enumflags
 {
