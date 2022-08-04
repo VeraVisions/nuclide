6ce196179276f8d8efac0ac9d1a6726aa04cc2a1
Marco Cawthorne <marco@vera-visions.com>
2022-08-03 18:41:11 -0700

    NSView: Document our header a bit more.

--- src/client/NSView.h
+++ src/client/NSView.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Vera Visions LLC.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 /* The job of a NSView is to contain a single player seat.
    Boxing is important for splitscreen etc. play.
 
@@ -34,15 +50,23 @@ NSView
 
 	void(void) NSView;
 
+	/* the only method we we want to call setproperty() */
 	virtual void(void) SetupView;
+	/* only does one thing: renderscene() */
 	virtual void(void) RenderView;
 
-	virtual void(viewmode_e) SetViewMode;
-	virtual viewmode_e(void) GetViewMode;
+	/* when called, will modify 'origin' to vertically smoothed when on ground */
 	virtual void(void) StairSmooth;
+
+	/* applies an punch to our camera angle, temporarily */
 	virtual void(vector) AddPunchAngle;
 
+	/* called every CSQC_UpdateView for each player */
 	virtual void(void) UpdateView;
+
+	/* set/get */
+	virtual void(viewmode_e) SetViewMode;
+	virtual viewmode_e(void) GetViewMode;
 
 	virtual void(vector) SetViewPosition;
 	virtual void(vector) SetViewSize;
@@ -64,5 +88,8 @@ NSView
 	virtual vector(void) GetCameraAngle;
 };
 
+/* one NSView for each seat */
 NSView g_viewSeats[4];
+
+/* the current seat run for each draw call, for each player. always use talk to this! */
 NSView g_view;
