a6da238e53472aba06b5bf5c7fdb71eaaf91459b
Marco Cawthorne <marco@vera-visions.com>
2022-08-24 14:44:16 -0700

    effects.h: Document EF_GREEN, which I missed.

--- src/shared/effects.h
+++ src/shared/effects.h
@@ -1,11 +1,27 @@
 
+/*
+ * Copyright (c) 2022 Vera Visions LLC.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
 /* engine reserved */
 #define EF_BRIGHTFIELD		(1<<0)
 #define EF_MUZZLEFLASH		(1<<1)
 #define EF_BRIGHTLIGHT		(1<<2)
 #define EF_DIMLIGHT			(1<<3)
-#define EF_NODRAW			(1<<4)
-#define EF_ADDITIVE			(1<<5)
+#define EF_NODRAW			(1<<4) /* also known as EF_FLAG1 in QW */
+#define EF_ADDITIVE			(1<<5) /* also known as EF_FLAG2 in QW */
 #define EF_BLUE				(1<<6)
 #define EF_RED				(1<<7)
 #define EF_UNUSED1			(1<<8)
@@ -18,9 +34,9 @@
 #define EF_UNUSED5			(1<<15)
 #define EF_UNUSED6			(1<<16)
 #define EF_UNUSED7			(1<<17)
-#define EF_UNUSED8			(1<<18)
-#define EF_UNUSED9			(1<<19)
-#define EF_UNUSED10			(1<<20)
-#define EF_UNUSED11			(1<<21)
-#define EF_UNUSED12			(1<<22)
-#define EF_UNUSED13			(1<<23)
+#define EF_GREEN			(1<<18)
+#define EF_UNUSED8			(1<<19)
+#define EF_UNUSED9			(1<<20)
+#define EF_UNUSED10			(1<<21)
+#define EF_UNUSED11			(1<<22)
+#define EF_UNUSED12			(1<<23)
