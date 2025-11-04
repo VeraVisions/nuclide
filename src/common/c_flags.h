862f4fdc2564eb7218b90dc925660eb5f5ebff7b
Marco Cawthorne <marco@icculus.org>
2025-11-03 17:03:22 -0800

    API: add TeamFlags implementation

--- src/common/c_flags.h
+++ src/common/c_flags.h
@@ -15,29 +15,29 @@
 */
 
 /* engine reserved */
-#define FL_FLY					(1<<0) /**< Entity is flying. */
-#define FL_SWIM					(1<<1) /**< Entity is swimming. */
-#define FL_GLIMPSE				(1<<2) /**< Unused? */
-#define FL_CLIENT				(1<<3) /**< Entity is a client. */
-#define FL_INWATER				(1<<4) /**< Entity is in water. */
-#define FL_MONSTER				(1<<5) /**< Entity is a monster. */
-#define FL_GODMODE				(1<<6) /**< Entity has 'god-mode' enabled. */
-#define FL_NOTARGET				(1<<7) /**< Entity has `notarget` enabled. */
-#define FL_ITEM					(1<<8) /**< Entity is an item. */
-#define FL_ONGROUND				(1<<9) /**< Entity is standing on ground. */
+#define FL_FLY				(1<<0) /**< Entity is flying. */
+#define FL_SWIM				(1<<1) /**< Entity is swimming. */
+#define FL_GLIMPSE			(1<<2) /**< Unused? */
+#define FL_CLIENT			(1<<3) /**< Entity is a client. */
+#define FL_INWATER			(1<<4) /**< Entity is in water. */
+#define FL_MONSTER			(1<<5) /**< Entity is a monster. */
+#define FL_GODMODE			(1<<6) /**< Entity has 'god-mode' enabled. */
+#define FL_NOTARGET			(1<<7) /**< Entity has `notarget` enabled. */
+#define FL_ITEM				(1<<8) /**< Entity is an item. */
+#define FL_ONGROUND			(1<<9) /**< Entity is standing on ground. */
 #define FL_PARTIALGROUND		(1<<10) /**< Entity is (partially) on ground. */
 #define FL_WATERJUMP			(1<<11) /**< Entity is within a water-jump moment. */
 #define FL_JUMPRELEASED			(1<<12) /**< Entity has let go off the jump button. */
-#define FL_FINDABLE_NONSOLID	(1<<14) /**< Entity is findable by find() calls, even when not-solid. */
+#define FL_FINDABLE_NONSOLID		(1<<14) /**< Entity is findable by find() calls, even when not-solid. */
 #define FLQW_LAGGEDMOVE			(1<<16) /**< Entity will move with lag compenstation. */
 
 /* nuclide */
 .float m_nuclideFlags;
-#define VFL_PRONE				(1<<0) /**< Entity is prone. */
+#define VFL_PRONE			(1<<0) /**< Entity is prone. */
 #define VFL_ONLADDER			(1<<1) /**< Entity is attached to a ladder. */
 #define VFL_CROUCHING			(1<<2) /**< Entity is crouching. */
 #define VFL_INVEHICLE			(1<<3) /**< Entity is inside a vehicle. */
-#define VFL_FROZEN				(1<<4) /**< Entity is not allowed to move. */
+#define VFL_FROZEN			(1<<4) /**< Entity is not allowed to move. */
 #define VFL_USE_RELEASED		(1<<5) /**< Entity has release the +use button. */
 #define VFL_FAKESPEC			(1<<6) /**< Entity is a fake spectator. */
 #define VFL_ONUSABLE			(1<<7) /**< Entity is able to +use a thing. */
@@ -45,8 +45,16 @@
 #define VFL_GOALITEM			(1<<9) /**< Entity is a goal-item. */
 #define VFL_NOATTACK			(1<<10) /**< Entity is not allowed to fire. */
 #define VFL_PRIMEDFUSE			(1<<11) /**< Entity is not allowed to fire. */
-#define VFL_REDRAW				(1<<12) /**< Entity is not allowed to fire. */
-#define VFL_FIRING				(1<<13) /**< Entity is firing. */
+#define VFL_REDRAW			(1<<12) /**< Entity is not allowed to fire. */
+#define VFL_FIRING			(1<<13) /**< Entity is firing. */
 #define VFL_VIDREMOVE			(1<<14) /**< Entity will be flushed upon vid_reload. */
 #define VFL_NOFRAMEADVANCE		(1<<15) /**< Entity will be requested not to advance in animation by itself. */
-#define VFL_HOLDING				(1<<16) /**< Entity will be requested not to advance in animation by itself. */
+#define VFL_HOLDING			(1<<16) /**< Entity will be requested not to advance in animation by itself. */
+
+/* team flags, set by teams.SetFlags() and teams.Flags() retrieves them. */
+#define TEAMFLAG_DEFAULT		0i	/**< Default. No special flags. */
+#define TEAMFLAG_OTHERSCOREBLIND	1i	/**< Unable to see the scores of the other team's players. Their individual scores will be unknown. */
+#define TEAMFLAG_SELFSCOREBLIND		2i	/**< Unable to see the scores of your own team its players. */
+#define TEAMFLAG_SCOREBLIND		(TEAMFLAG_OTHERSCOREBLIND | TEAMFLAG_SELFSCOREBLIND)	/**< Unable to see the scores of both teams. */
+#define TEAMFLAG_TEAMBLIND		4i	/**< Unable to see the team associations. To them the game appears like it does in something like *Deathmatch* or *Free For All* type game modes. */
+#define TEAMFLAG_KILLMSGBLIND		8i	/**< Unable to receive kill messages. */
